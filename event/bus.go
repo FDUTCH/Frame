ecd0e5b78bf67326daefc1ca30cb5e1a8f0408d5
Alexander <123891354+FDUTCH@users.noreply.github.com>
2026-07-27 15:53:13 +0200

    Improve perf

--- event/bus.go
+++ event/bus.go
@@ -9,13 +9,13 @@ import (
 // Bus is a thread-safe bus of events.
 type Bus struct {
 	mu       sync.RWMutex
-	handlers map[reflect.Type][]any
+	handlers map[reflect.Type]any
 }
 
 // NewBus creates new Bus.
 func NewBus() *Bus {
 	return &Bus{
-		handlers: make(map[reflect.Type][]any),
+		handlers: make(map[reflect.Type]any),
 	}
 }
 
@@ -25,7 +25,12 @@ func Subscribe[T any](bus *Bus, listener func(ev T)) {
 	defer bus.mu.Unlock()
 
 	eventType := reflect.TypeOf((*T)(nil)).Elem()
-	bus.handlers[eventType] = append(bus.handlers[eventType], listener)
+	handlers, ok := bus.handlers[eventType]
+	if ok {
+		bus.handlers[eventType] = append(handlers.([]func(T)), listener)
+	} else {
+		bus.handlers[eventType] = []func(T){listener}
+	}
 }
 
 // Unsubscribe unsubscribes from event.
@@ -36,7 +41,11 @@ func Unsubscribe[T any](bus *Bus, fn func(listener T)) {
 	// there a way to shoot your self in the foot, but idc.
 	eventType := reflect.TypeOf((*T)(nil)).Elem()
 	addr := reflect.ValueOf(fn).UnsafeAddr()
-	bus.handlers[eventType] = slices.DeleteFunc(bus.handlers[eventType], func(a any) bool {
+	handlers, ok := bus.handlers[eventType]
+	if !ok {
+		return
+	}
+	bus.handlers[eventType] = slices.DeleteFunc(handlers.([]func(T)), func(a func(T)) bool {
 		return addr == reflect.ValueOf(a).UnsafeAddr()
 	})
 }
@@ -51,14 +60,11 @@ func Publish[T any](bus *Bus, event T) {
 		return
 	}
 
-	handlers := slices.Clone(rawHandlers)
+	handlers := slices.Clone(rawHandlers.([]func(T)))
 	bus.mu.RUnlock()
 
-	// this may be improved with unsafe stuff, but would that actually become a bottleneck?
 	for _, h := range handlers {
-		if fn, ok := h.(func(T)); ok {
-			fn(event)
-		}
+		h(event)
 	}
 }
 
